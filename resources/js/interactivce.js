55a8af05e1fae2826551a3540ab642535e3465f9
Lachlan Moore <lachlan@moore.moe>
2025-02-03 00:17:28 +1100

    added some info plus start of javascript expand module

--- resources/js/interactivce.js
+++ resources/js/interactivce.js
@@ -14,3 +14,9 @@ function gotLocation(pos) {
 function noLocationFound(error) {
     console.warn("No Location could be found " + error)
 }
+
+//add info about this into the website "Learning to use a computer in kingergarten, we were always told to double click to access websites, so that functionality has made it to today"
+//implement double click functionality -- when expanded, turn link active, otherwise not (so it expands first)
+function expandDomain() {
+    console.log("worked")
+}
